8d9529cf8140d7db4db17c946aff9ee426519214
Krishs2006 <dearkrish2006i@gmail.com>
2026-09-20 21:58:08 +0530

    Day 2: Add Majority Element solution

---
+++ Day-02/MajorityElement.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (count == 0) {
+                candidate = nums[i];
+            }
+
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(majorityElement(nums));
+    }
+}
